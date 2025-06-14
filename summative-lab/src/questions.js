f38a29f44697c65770f2e5210a61ea220a4b5f9a
Nick Rathbone <nrathbone@joinhandshake.com>
2025-06-13 22:18:43 -0600

    Create separate file to store questions as objects with choices and answers

---
+++ summative-lab/src/questions.js
@@ -0,0 +1,14 @@
+const questions = [
+    {
+        question: "What is 2 + 2?",
+        choices: ["3", "4", "5"],
+        answer: "4"
+    },
+    {
+        question: "What is the capital of France?",
+        choices: ["Berlin", "Madrid", "Paris"],
+        answer: "Paris"
+    }
+];
+
+module.exports = questions;
